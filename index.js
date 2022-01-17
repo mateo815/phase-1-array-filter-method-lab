5b6605cf3a7a9a8af0f7a419ef2dae0d60b82b37
Matt Quinn <mjq0533@gmail.com>
2022-01-17 09:36:17 -0600

    commit tree 9a2e7e0ceb136c193b3e512eb3d59ff3710eff2b

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // Code your solution here
-function findMatching(source, sought) {
-    return source.filter(
-      (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
-    );
-  }
-  
-  function fuzzyMatch(source, testString) {
-    return source.filter(
-      (possibleMatch) =>
-        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
-    );
-  }
-  
-  function matchName(source, soughtName) {
-    return source.filter((record) => record.name === soughtName);
-  }
+
+function findMatching(drivers, string) {
+  return drivers.filter(
+    (match) => match.toLowerCase() === string.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(
+    (match) =>
+      match.toLowerCase().indexOf(string.toLowerCase()) === 0
+  );
+
+}
+
+function matchName(drivers, string) {
+  return drivers.filter((record) => record.name === string);
+}
